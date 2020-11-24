b403e9cc8bb0b41e2df9ec1559cac21b5b441411
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-24 11:44:05 -0600

    commit tree 8d85bff280c67b85f6eab52114d4bfd017aeb2a4

---
+++ lib/project.rb
@@ -0,0 +1,23 @@
+class Project
+    attr_reader :title
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        project_backers = ProjectBacker.all.select do |pb|
+            pb.project == self
+        end
+        project_backers.map do |pb|
+            pb.backer
+        end
+    end
+end
